9c8064296e313e79cc58947ace5a51e334cd4c68
EmerBV <emersmc@hotmail.com>
2023-09-16 20:29:30 +0200

    + TMDBMainTabBarVC implemented

--- TMDBApp/Controllers/Core/TMDBMainTabBarViewController.swift
+++ TMDBApp/Controllers/Core/TMDBMainTabBarViewController.swift
@@ -11,19 +11,29 @@ final class TMDBMainTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        setUpTabs()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setUpTabs() {
+        let vc1 = UINavigationController(rootViewController: TMDBHomeViewController())
+        let vc2 = UINavigationController(rootViewController: TMDBUpcomingViewController())
+        let vc3 = UINavigationController(rootViewController: TMDBSearchViewController())
+        let vc4 = UINavigationController(rootViewController: TMDBDownloadsViewController())
+        
+        vc1.tabBarItem.image = UIImage(systemName: "house.circle")
+        vc2.tabBarItem.image = UIImage(systemName: "play.circle")
+        vc3.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        vc4.tabBarItem.image = UIImage(systemName: "arrow.down.circle")
+        
+        vc1.title = "Home"
+        vc2.title = "Coming Soon"
+        vc3.title = "Top Search"
+        vc4.title = "Downloads"
+        
+        tabBar.tintColor = .label
+        
+        setViewControllers([vc1, vc2, vc3, vc4], animated: true)
     }
-    */
 
 }
