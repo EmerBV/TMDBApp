6883531eb7ca5b5f9af8a2f921cf0773220be054
EmerBV <emersmc@hotmail.com>
2023-09-16 20:21:31 +0200

    Initial Commit

---
+++ TMDBApp/Controllers/Core/TMDBMainTabBarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TMDBMainTabBarViewController.swift
+//  TMDBApp
+//
+//  Created by Emerson Balahan Varona on 16/9/23.
+//
+
+import UIKit
+
+final class TMDBMainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TMDBApp/Controllers/Core/TMDBSearchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TMDBSearchViewController.swift
+//  TMDBApp
+//
+//  Created by Emerson Balahan Varona on 16/9/23.
+//
+
+import UIKit
+
+class TMDBSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
