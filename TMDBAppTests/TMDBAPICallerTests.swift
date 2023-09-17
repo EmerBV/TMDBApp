7dcd6fc738c02d008b23645387a5734f4de2c4b2
EmerBV <emersmc@hotmail.com>
2023-09-17 19:03:15 +0200

    + API calls test passed

--- TMDBAppTests/TMDBAPICallerTests.swift
+++ TMDBAppTests/TMDBAPICallerTests.swift
@@ -9,28 +9,49 @@ import XCTest
 @testable import TMDBApp
 
 final class TMDBAPICallerTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    let apiCaller = TMDBAPICaller.shared
+    
+    override func setUp() {
+        super.setUp()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDown() {
+        super.tearDown()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testGetTrendingMovies() {
+        let expectation = XCTestExpectation(description: "Espera la respuesta de getTrendingMovies")
+        
+        apiCaller.getTrendingMovies { result in
+            switch result {
+            case .success(let titles):
+                XCTAssertFalse(titles.isEmpty)
+            case .failure(let error):
+                XCTFail("Error al obtener las películas: \(error.localizedDescription)")
+            }
+            
+            expectation.fulfill()
         }
+        
+        wait(for: [expectation], timeout: 10.0)
     }
-
+    
+    func testGetTrendingTvs() {
+        let expectation = XCTestExpectation(description: "Espera la respuesta de getTrendingTvs")
+        
+        apiCaller.getTrendingTvs { result in
+            switch result {
+            case .success(let titles):
+                XCTAssertFalse(titles.isEmpty)
+            case .failure(let error):
+                XCTFail("Error al obtener las películas: \(error.localizedDescription)")
+            }
+            
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 10.0)
+    }
+    
 }
